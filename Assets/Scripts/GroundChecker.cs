d46d30baf2813ab8aa2bca8da89cdce5170d1751
Isac Lagerström <isac.lagerstrom@gmail.com>
2022-01-27 11:01:36 +0100

    Added Groundchecker for jumping

---
+++ Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+   public bool Isgrounded { get; private set; }
+   [SerializeField] private float groundCheckLength = 1f;
+   [SerializeField] private float groundCheckRadius = 0.5f;
+   [SerializeField] private LayerMask groundLayers;
+    void Update()
+    {
+        var ray = new Ray(transform.position, Vector3.down);
+        Isgrounded = Physics.SphereCast(ray, groundCheckRadius,groundCheckLength);
+        
+        Debug.DrawRay(transform.position, Vector3.down * groundCheckLength, Color.magenta);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position + Vector3.down * groundCheckLength, groundCheckRadius);
+    }
+}
--- Assets/Scripts/PlayerImmediateJumpController.cs
+++ Assets/Scripts/PlayerImmediateJumpController.cs
@@ -5,11 +5,12 @@ public class PlayerImmediateJumpController : MonoBehaviour
     [SerializeField] private PlayerInputController playerInputController;
     [SerializeField] private Rigidbody myRigidbody;
     [SerializeField] private float jumpForce = 500f;
+    [SerializeField] private GroundChecker groundChecker;
     void Update()
     {
         
         
-        if (playerInputController.jumpInput && myRigidbody.velocity.y == 0)
+        if (playerInputController.jumpInput && groundChecker.Isgrounded)
         {
             myRigidbody.AddForce(Vector3.up * jumpForce);
         }
